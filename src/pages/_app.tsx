518040d1d04575d134db074705bfbf749ca7b8bc
Leo Miranda <contato@leomiranda.com>
2021-09-01 22:09:59 -0400

    fix(material-ui): creating document next override

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
-/* eslint-disable @next/next/no-page-custom-font */
+import { useEffect } from 'react';
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		document.querySelector('#jss-server-side')?.remove();
+	}, []);
+
 	return (
 		<>
 			<Head>
 				<title>e-diaristas</title>
-				<link
-					rel='stylesheet'
-					href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-				/>
 			</Head>
 			<Component {...pageProps} />
 		</>
---
+++ src/pages/_document.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Document, {
+	Html,
+	Head,
+	Main,
+	NextScript,
+	DocumentContext,
+	DocumentInitialProps,
+} from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
+
+export default class MyDocument extends Document {
+	static async getInitialProps(
+		ctx: DocumentContext
+	): Promise<DocumentInitialProps> {
+		const sheets = new ServerStyleSheets(),
+			originalRenderPage = ctx.renderPage;
+
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+    
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return {
+      ...initialProps,
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement()
+      ]
+		};
+	}
+
+	render(): JSX.Element {
+		return (
+			<Html>
+				<Head>
+					<link
+						rel='stylesheet'
+						href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}

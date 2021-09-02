22d35ea931f6db717c2e754abb638a857a935414
Leo Miranda <contato@leomiranda.com>
2021-09-01 21:29:28 -0400

    style: add material-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable @next/next/no-page-custom-font */
+import '@styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>e-diaristas</title>
+				<link
+					rel='stylesheet'
+					href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
-export default MyApp
+export default MyApp;
